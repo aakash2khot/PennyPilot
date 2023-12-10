6f3ad3264c78bfe309ea036af46c11ee453f4d0b
Aakash Khot <aakash.khot02@gmail.com>
2023-12-10 22:43:28 +0530

    mongo conn...

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -4,7 +4,8 @@ mongoose.set('strictQuery', false);
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    // await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb://mongo:27017/PennyPilot");
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
