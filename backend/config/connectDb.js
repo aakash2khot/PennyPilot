e08d473eb42111be9d474cc57ba9448ab2b93625
Aakash Khot <aakash.khot02@gmail.com>
2023-12-10 23:37:46 +0530

    mongo conn...

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -5,7 +5,7 @@ const colors = require("colors");
 const connectDb = async () => {
   try {
     // await mongoose.connect(process.env.MONGO_URL);
-    await mongoose.connect("mongodb://ash:123@mongodb:27017/dp");
+    await mongoose.connect("mongodb://ash:123@mongodb:27017/db");
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
