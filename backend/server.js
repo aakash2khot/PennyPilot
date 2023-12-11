f9c36acdd27b286974d5fdc5f533e28681013909
Aakash Khot <aakash.khot02@gmail.com>
2023-12-11 08:54:37 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 //routes
 //user routes
