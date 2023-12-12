c58b229d4021952b327a3021c88b5a90637cca5e
Aakash Khot <aakash.khot02@gmail.com>
2023-12-12 09:41:27 +0530

    minor changes made

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -7,7 +7,7 @@ const connectDb = async () => {
     // await mongoose.connect(process.env.MONGO_URL);
     await mongoose.connect("mongodb://mongo:27017/expenseApp");
     // await mongoose.connect("mongodb+srv://ash:123@cluster0.eioachd.mongodb.net/expenseApp")
-    console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
+    console.log(`Server Running On mongo --  ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
     console.log("not connectd to mongoose")
