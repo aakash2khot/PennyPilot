337aa9bf1816b8ed64a4952e1aeb767dbc1a0721
Aakash Khot <aakash.khot02@gmail.com>
2023-12-12 09:53:49 +0530

    minor changes made

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
+
 
 const colors = require("colors");
 const connectDb = async () => {
   try {
+    const mongoose = require("mongoose");
+    mongoose.set('strictQuery', false);
     // await mongoose.connect(process.env.MONGO_URL);
     await mongoose.connect("mongodb://mongo:27017/expenseApp");
     // await mongoose.connect("mongodb+srv://ash:123@cluster0.eioachd.mongodb.net/expenseApp")
