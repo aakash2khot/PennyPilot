c90b09f4415297e9e80bf53fd572c9322319e393
Aakash Khot <aakash.khot02@gmail.com>
2023-12-12 09:08:08 +0530

    changes in docker compose

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -5,7 +5,7 @@ const colors = require("colors");
 const connectDb = async () => {
   try {
     // await mongoose.connect(process.env.MONGO_URL);
-    await mongoose.connect("mongodb://mongodb:27017/PennyPilot", { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect("mongodb://mongo:27017/expenseApp");
     // await mongoose.connect("mongodb+srv://ash:123@cluster0.eioachd.mongodb.net/expenseApp")
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
