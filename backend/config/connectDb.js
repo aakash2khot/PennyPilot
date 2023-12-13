4a19eaa5b9272fa9b990c295b766c584d610b47e
Aakash Khot <aakash.khot02@gmail.com>
2023-12-13 19:46:56 +0530

    Checking jenkins

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false);
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    const URL = process.env.NODE_ENV === 'test' ? process.env.MONGO_TEST_URL : process.env.MONGO__URL;
+    const URL = process.env.NODE_ENV === 'test' ? process.env.MONGO_TEST_URL : process.env.MONGO_URL;
     await mongoose.connect(URL);
     // await mongoose.connect("mongodb+srv://ash:123@cluster0.eioachd.mongodb.net/expenseApp");
     console.log(`Server Running On mongo --  ${mongoose.connection.host}`.bgCyan.white);
