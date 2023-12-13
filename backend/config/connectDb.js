d1f7009f4459597930a5ef26cce3062b29d0fed9
Aakash Khot <aakash.khot02@gmail.com>
2023-12-13 17:49:00 +0530

    changes for deployment of backend

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -6,7 +6,8 @@ const colors = require("colors");
 const connectDb = async () => {
   try {
     const URL = process.env.NODE_ENV === 'test' ? process.env.MONGO_TEST_URL : process.env.MONGO__URL
-    await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true});
+    // await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect("mongodb+srv://ash:123@cluster0.eioachd.mongodb.net/expenseApp");
     console.log(`Server Running On mongo --  ${mongoose.connection.host}`.bgCyan.white);
     logger.info("Connected to Mongo Database");
   } catch (error) {
