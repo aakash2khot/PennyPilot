30b47a9a7237fe6bad5c602558998f8416088aa4
Aakash Khot <aakash.khot02@gmail.com>
2023-12-14 21:42:19 +0530

    changes

--- backend/tests/TestFile.test.js
+++ backend/tests/TestFile.test.js
@@ -62,8 +62,8 @@ describe('User Registration', () => {
     test('should register a new user', async () => {
       const nuser = ({
         password: "3412",
-        email: "huuuyii@gmail.com",
-        name: "kol michalson"
+        email: "rrri@gmail.com",
+        name: "kolrrrr michalson"
       });
       
 
